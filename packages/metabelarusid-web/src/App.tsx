20f91e9e0adcb7bc4c4a388112ee3d45d70dee04
Mark Vandal <araht.pareen@gmail.com>
2021-08-13 20:42:06 +0200

    Some initially working web wallet basis is initiated

---
+++ packages/metabelarusid-web/src/App.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import { AffinidiWallet } from '@affinidi/wallet-browser-sdk'
+import logo from './logo.svg';
+import './App.css';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+function App() {
+  console.log(AffinidiWallet)
+
+  return (
+    <Container maxWidth="sm">
+      <Box my={4} className="App-header">
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v4-beta example
+        </Typography>
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
+
+export default App;
