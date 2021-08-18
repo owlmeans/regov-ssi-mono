83594deeb9c138a1e0f1d4b0d486bb9a64432058
Mark Vandal <araht.pareen@gmail.com>
2021-08-18 19:09:19 +0200

    Removed custom-core from the repo

--- packages/core-custom/wallet/issuer.ts
+++
@@ -1,6 +0,0 @@
-import { IssuerWalletOperator, Wallet, WalletContext } from "./types";
-
-export const produceWalletIssuer =
-(wallet: Wallet, context: WalletContext): () => IssuerWalletOperator =>
-  () => ({
-  })
